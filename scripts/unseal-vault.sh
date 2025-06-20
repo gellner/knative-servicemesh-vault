04e51f8b86f6a02a34b8c436f34d4cdcd33753ad
Gareth Ellner <29887711+gellner@users.noreply.github.com>
2025-06-20 08:39:12 +0100

    Add unseal script

---
+++ scripts/unseal-vault.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run this from the directory that contains the init-keys.json file, while logged into oc cli.
+
+# export VAULT_ADDR=https://vaultui.apps.cluster1.example.com
+# export VAULT_CAPATH="/home/user/clusters/cluster1/ingress.pem"   ## ONLY if route is self-signed
+
+# Set environmental variables for unseal key and root token
+echo "Setting environmental variables for the Unseal Key and the Root Token"
+export VAULT_UNSEAL_KEY=$(cat init-keys.json | jq -r ".unseal_keys_b64[]")
+export VAULT_ROOT_TOKEN=$(cat init-keys.json | jq -r ".root_token")
+echo "VAULT_UNSEAL_KEY = $VAULT_UNSEAL_KEY"
+echo "VAULT_ROOT_TOKEN = $VAULT_ROOT_TOKEN"
+
+
+oc exec vault-0 -n vault -- vault operator unseal $VAULT_UNSEAL_KEY
